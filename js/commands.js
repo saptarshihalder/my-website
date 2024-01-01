174615c046b49e56137a872d1a06eb7406671ceb
Saptarshi Halder <62690091+saptarshihalder@users.noreply.github.com>
2024-01-01 22:18:10 +0530

    Add files via upload

---
+++ js/commands.js
@@ -0,0 +1,131 @@
+var twitter = "https://twitter.com/Iamsaptarshi69";
+var linkedin = "https://www.linkedin.com/in/saptarshi-halder/";
+var youtube = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+var github = "https://github.com/saptarshihalder";
+var sudo = "https://www.youtube.com/watch?v=dQw4w9WgXcQ?autoplay=1";
+var email = "mailto:saptarshihalder69@gmail.com";
+
+aboutme = [
+  "<br>",
+  "<span class='underline'>Hey, I'm Saptarshi!👋</span>",
+  "<br>",
+  "<li>🔭 I’m currently a 2nd year undergrad, pursuing Bachelors in Electrical Engineering And Computer Science At Indian Institute Of Science Education And Research.</li>",
+  "<li>🌱 My fields of intetest are</li>",
+  `<pre class="whitespace-pre-wrap">
+        - Fullstack Development
+        - AI/ML Research And Development
+        - Application Development</pre>`
+       ,
+  `<li><pre class="whitespace-pre-wrap">🤝 I am a full stack developer who likes to explore new technologies and learn. I am an AI/ML enthusiast and love to solve problems that deals with complicated real life scenerios. I dream of creating applications that would help in driving the world of technology towards a brighter future
+   You can browse around my website to find out more about 
+   me.
+   *Try using a social command.`,
+  `<li><pre class="whitespace-pre-wrap">👨‍💻 Check out my github profile to view my projects
+   *Use the github command to take a look at my GitHub profile.`,
+  `<li>📫 Contact me at <a href= "https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=saptarshihalder69@gmail.com">saptarshihalder69@gmail.com</a>`,
+  "<br>",
+];
+
+social = [
+  "<br>",
+  ,
+  'twitter        <a href="' +
+    twitter +
+    '" target="blank">Check Out My Twitter' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="blank">Check Out My Linkedin' +
+    "</a>",
+  'youtube        <a href="' +
+    youtube +
+    '" target="blank">Check Out My Youtube' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="blank">Check Out My GitHub' +
+    "</a>",
+  
+  "<br>",
+];
+hack = [
+  "Initializing hacking sequence... 0%",
+  "Connecting to main server... 10%",
+  "Connection failed. Retrying... 10%",
+  "Connected!! Injecting malicious code... 20%",
+  "Bypassing firewall... 30%",
+  "Firewall successfully bypassed!!. Proceeding with the operation... 30%",
+  "Executing zero-day exploits... 40%",
+  "Deciphering firewall defenses... 50%",
+  "Downloading classified data... 60%",
+  "File decrypted successfully. Accessing sensitive information....60%",
+  "System detected counter-hacking attempt. Initiating evasive maneuvers...",
+  "Setting up VPN connection...",
+  "VPN established. Anonymity secured. Proceed with operations....70%",
+  "Accessing sensitive information.... 75%",
+  "Gathering Minecraft account and Vbucks account details... 80%",
+  "Creating a backdoor for persistent access.... 80%",
+  "Backdoor successfully created. Accessible anytime.",
+  "        ",
+  "Database contents:",
+  " - User: Admin",
+  " - Password: *********",
+  " Port 21 (FTP) - Vulnerable (- Cracked)",
+ " Port 80 (HTTP) - Secure",
+  "Port 445 (SMB) - Vulnerable (- Cracked)",
+  " Minecraft Password: ********",
+  " Minecraft Username: ********",
+  " Minecraft UUID: ********",
+  " Minecraft Skin: ********",
+  " Vbucks Account: ********",
+  " Vbucks Username: ********",  
+  " Vbucks Password: ********",
+  " Vbucks Email: ********",
+  " Vbucks Phone Number: ********",
+  " Bank Details: Unavailable",
+  " Credit Card Details: Unavailable",
+  "Initiating scheme two protocol... 90%",
+  "Retrieving personal details of the account holder... 100%",
+  "Generating Brute Force algorithm to bypass keysafe encryption.... 100%",
+  "Brute Force attack successful. Accessing sensitive information... 100%",
+  "Surprise! Your account has been hacked!! Jk. Its a joke",
+];
+
+projects = [
+  "<br>",
+  `<div style="height: 0; padding-bottom: calc(20.00%); position:relative; width: %;"><iframe allow="autoplay; gyroscope;" allowfullscreen height="20%" referrerpolicy="strict-origin" src="https://www.kapwing.com/e/659060649b6145eef69f918e" style="border:0; height:20%; left:0; overflow:hidden; position:absolute; top:0; width:20%" title="STay Tuned" width="20%"></iframe></div><p style="font-size: 12px; text-align: right;"></p>`,
+  "<br>",
+];
+
+education = [
+  "🏫 Highschool: Don Bosco — High school education a focus on academics and co-curricular activities [2009 - 2020]",
+  "📚 Higher Secondary: Central Academy — Higher secondary education from Central Academy with a focus on preparing for specialized fields of study in Science [2020-2022]",
+  "🎓 Bachelors In EECS: Indian Institute of Science Education And Research Bhopal — Pursuing a Bachelors in Electrical Engineering and Computer Science at IISER Bhopal [2022-2026]"
+];
+
+help = [
+  `<br><pre class="whitespace-pre-wrap">
+  <span class="command">aboutme</span>    
+   <span class="command">↳</span>&nbsp;Diplays Who I am?
+  <span class="command">social</span>     
+   <span class="command">↳</span>&nbsp;Lists social networks.
+  <span class="command">projects</span>   
+   <span class="command">↳</span>&nbsp;View coding projects.
+   <span class="command">education</span>   
+    <span class="command">↳</span>&nbsp;Shows My Education Level.
+   <span class="command">hack</span>   
+      <span class="command">↳</span>&nbsp;Initiate Hack.
+  <span class="command">email</span>      
+   <span class="command">↳</span>&nbsp;To send me a email.
+  <span class="command">history</span>    
+   <span class="command">↳</span>&nbsp;View command history.
+  <span class="command">help</span>       
+   <span class="command">↳</span>&nbsp;Displays this help msg.
+  <span class="command">sudo</span>       
+   <span class="command">↳</span>&nbsp;Try it out for yourself.
+  <span class="command">clear</span>
+   <span class="command">↳</span>&nbsp;Clear terminal.</pre><br>`,
+];
+banner = [
+  `<div class="pt-2"><span class="text-[#7d82d7db] ">Welcome to my portfolio! — Type <span class="command text-[#75e1e7]">help</span> for a list of supported commands.</span></div>`,
+];
